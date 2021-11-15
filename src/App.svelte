ed749c2115822df5523cf5900a44f806d5d9585f
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 19:00:11 +0530

    Makkah masjid , bilwardahalli, bangalore

--- src/App.svelte
+++ src/App.svelte
@@ -4,26 +4,13 @@
 <main>
 	<h1>Search Masjid</h1>
 	<p>Add , edit, delete, list, search Masjid</p>
+
+    <h1>Makkah Masjid & Madrasa Habeeb</h1>
+	<p>
+		Address: Bengaluru,, Pillaganahalli, Bengaluru, Karnataka 560083,
+		<a href='https://www.google.com/maps/dir//makka+masjid+bilwardahalli/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bae6a8c2cb7faad:0xe6c0728ec867eea1?sa=X&ved=2ahUKEwjqzMq-vJr0AhW-wjgGHW3EC8oQ9Rd6BAhmEAM'>google map</a>
+	</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
